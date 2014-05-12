07a74fe8fa59ace95231e25f35845ddd57fb8ea2
Olga Helena <oh.cardenas@gmail.com>
2014-05-12 08:55:50 -0500

    Unlocked when meets goal condition

---
+++ Assets/Editor/AchievementTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace UnityTest
+{
+	internal class AchievementTest
+	{
+		private Achievement achievement;
+
+		[Test]
+		public void UnlockedWhenMeetsGoalCondition ()
+		{
+			achievement = new Achievement ("a123", "kill", 4, 5);
+			achievement.Progress++;
+
+			Assert.IsTrue (achievement.IsUnlocked);
+		}
+	}
+}
---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,39 @@
+﻿public class Achievement : IAchievement
+{
+	public string Id 
+	{ 
+		get;
+		private set;
+	}
+
+	public string Type 
+	{ 
+		get;
+		private set;
+	}
+
+	public int Progress
+	{ 
+		get;
+		set;
+	}
+
+	public int Goal
+	{ 
+		get;
+		private set;
+	}
+	
+	public bool IsUnlocked
+	{ 
+		get { return Progress >= Goal; }
+	}
+
+	public Achievement (string id, string type, int initial, int goal)
+	{
+		Id = id;
+		Type = type;
+		Progress = initial;
+		Goal = goal;
+	}
+}
