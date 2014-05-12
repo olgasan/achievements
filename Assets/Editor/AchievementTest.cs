41ad5349db2feaaab0857aa953faf5c221327e5e
Olga Helena <oh.cardenas@gmail.com>
2014-05-11 19:33:04 -0500

    Added unity3d uint testing tools

---
+++ Assets/Editor/AchievementTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace UnityTest
+{
+	internal class AchievementTest 
+	{
+		private Achievement achievement;
+
+		[Test]
+		public void Create ()
+		{
+			achievement = new Achievement ();
+		}
+	}
+}
