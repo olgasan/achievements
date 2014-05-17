b6f981131e989c5b4299060cc62d0ebcfb64e757
Olga Cardenas <oh.cardenas@gmail.com>
2014-05-16 19:38:15 -0500

    cleaning refactorç

--- Assets/Editor/Achievements/GamingNetworkTest.cs
+++ Assets/Editor/Achievements/GamingNetworkTest.cs
@@ -7,45 +7,45 @@ namespace UnityTest
 	internal class GamingNetworkTest
 	{
 		private AchievementFaker faker = new AchievementFaker ();
-
+		private Achieve achieve;
+		private IGamingNetworkAdapter adapter;
+		private GamingNetwork network;
+		
+		[SetUp]
+		public void SetUp ()
+		{
+			achieve = new Achieve ();
+			adapter = faker.CreateFakeGamingNetworkAdapter ();
+			network = new GamingNetwork (achieve, adapter);
+		}
+		
 		[Test]
 		public void NotifyNetworkWhenAchievementIsRegistered ()
 		{
-			Achieve achieve = new Achieve ();
-			IGamingNetworkAdapter adapter = faker.CreateFakeGamingNetworkAdapter ();
-			GamingNetwork network = new GamingNetwork (achieve, adapter);
-
 			IAchievement registered = faker.CreateFakeAchievement ("a123", "kill", 1, 100);
 			achieve.Register (registered);
-
+			
 			Assert.IsTrue (network.Achievements.Contains (registered));
 			adapter.Received ().Register (Arg.Any<IAchievement>());
 		}
-
+		
 		[Test]
 		[Ignore]
 		public void NotifyNetworkWhenAchievementIsUnlocked ()
 		{
-			Achieve achieve = new Achieve ();
-			IGamingNetworkAdapter adapter = faker.CreateFakeGamingNetworkAdapter ();
-			new GamingNetwork (achieve, adapter);
-			
 			IAchievement registered = faker.CreateFakeAchievement ("a123", "kill", 0, 1);
 			achieve.Register (registered);
 			achieve.OnEvent ("kill");
-
+			
 			//Assert Unlocked was called
 			adapter.Received ().Unlocked (Arg.Any<IAchievement>());
 		}
-
+		
 		[Test]
 		public void InitializeAdapter ()
 		{
-			Achieve achieve = new Achieve ();
-			IGamingNetworkAdapter adapter = faker.CreateFakeGamingNetworkAdapter ();
-			GamingNetwork network = new GamingNetwork (achieve, adapter);
 			network.Init ();
-
+			
 			//Assert Init was called
 			adapter.Received ().Init ();
 		}
